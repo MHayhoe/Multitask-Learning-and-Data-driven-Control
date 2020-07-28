c12abb11d5fb138e6346800ca38dd348d6fff6fc
Mikhail Hayhoe <m.hayhoe@icloud.com>
2020-07-28 11:24:07 -0400

    Updating dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -16,61 +16,71 @@ df = import_county_data()
 df_deaths = import_deaths_data()
 fig_map = make_map_figure(df_deaths)
 fig_series = make_deaths_figure(df_deaths)
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+    html.Div(id='hover-data', style={'display': 'none'}),
+    html.Div(id='select-data', style={'display': 'none'}),
     html.Div([
-        dcc.Dropdown(
-            id='state-dropdown',
-            options=[{'label': sa, 'value': sn} for (sa, sn) in zip(state_names, state_abbrevs)],
-            value='PA')
-    ], style={'width': '48%', 'display': 'inline-block'}),
+        html.Div([
+    html.Div([
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id='state-dropdown',
+                    options=[{'label': sa, 'value': sn} for (sa, sn) in zip(state_names, state_abbrevs)],
+                    value='', style={'width': '160px'})
+            ]),
 
-    html.Div([
-        dcc.Dropdown(id='county-dropdown')
-    ], style={'width': '48%', 'display': 'inline-block'}),
-
-    html.Div([
-        dcc.Graph(id='map', figure=fig_map, style={'height': '200px'})
-    ], style={'width': '48%', 'height': '250px', 'display': 'inline-block'}),
-
-    html.Div([
-        dcc.Graph(id='series-plot', figure=fig_series, style={'height': '200px'})
-    ], style={'width': '48%', 'height': '250px', 'display': 'inline-block'})
+            html.Div([
+                dcc.Graph(id='map', figure=fig_map, clear_on_unhover=True, config={'displayModeBar': False}, style={'height': '200px'})
+            ]),
+        ], id='map-container', className='column is-4', style={'padding': '10px'}),
+        html.Div([
+            dcc.Graph(id='series-plot', figure=fig_series, config={'displayModeBar': False}, style={'height': '200px'})
+        ], id='chart-container', className='column is-8')
+    ], className='columns'),
+    ], className='container')], className='section'),
+    html.Hr(style={'margin': '0px'}),
+    html.Footer([
+        html.Div([
+            html.Div(['Last updated on Tuesday, July 28th.'], className='content has-text-centered')
+        ], className='container')
+    ], className='footer')
 ])
+
+
+# # Change behaviour based on what is being hovered over
+# @app.callback(Output('hover-data', 'children'),
+#               [Input('map','hoverData')])
+# def update_hover_data(hover_data):
+#     if hover_data:
+#         return hover_data["points"][0]['location']
+#     else:
+#         return None
 
 
 # Set state based on clicking map
 @app.callback(Output('state-dropdown', 'value'),
-              [Input('map', 'clickData')])
-def set_state_on_map_click(click_data):
+              [Input('map', 'clickData'), Input('select-data', 'children')])
+def set_state_on_map_click(click_data, current_selected_state):
     if click_data:
         selection = click_data["points"][0]['location']
-        if selection in state_abbrevs:
+        if selection == current_selected_state:
+            return None
+        elif selection in state_abbrevs:
             return selection
     else:
         return None
 
 
-# Sets available options in the county dropdown when the state dropdown is changed
-@app.callback(Output('county-dropdown', 'options'),
-              [Input('state-dropdown', 'value')])
-def set_county_options(selected_state):
-    if selected_state:
-        df_state = df[df['STATE'] == selected_state]
-        return [{'label': 'Statewide', 'value': us.states.lookup(selected_state).fips}] + \
-               [{'label': name, 'value': fips} for (fips, name) in zip(df_state['FIPS'], df_state['NAME'])]
-    else:
-        return [{'label': 'National', 'value': 'US'}]
-
-
 # Highlights the map according to current selection
 @app.callback(Output('map', 'figure'),
-              [Input('state-dropdown', 'value')])
-def set_map_highlight(selected_state):
-    return make_map_figure(df_deaths, selected_state)
+              [Input('state-dropdown', 'value'), Input('hover-data', 'children')])
+def set_map_highlight(selected_state, hover_state):
+    return make_map_figure(df_deaths, selected_state, hover_state)
 
 
 # Updates plot according to current selection
@@ -80,11 +90,23 @@ def set_series_highlight(selected_state):
     return make_deaths_figure(df_deaths, selected_state)
 
 
+# Sets available options in the county dropdown when the state dropdown is changed
+# @app.callback(Output('county-dropdown', 'options'),
+#               [Input('state-dropdown', 'value')])
+# def set_county_options(selected_state):
+#     if selected_state:
+#         df_state = df[df['STATE'] == selected_state]
+#         return [{'label': 'Statewide', 'value': us.states.lookup(selected_state).fips}] + \
+#                [{'label': name, 'value': fips} for (fips, name) in zip(df_state['FIPS'], df_state['NAME'])]
+#     else:
+#         return [{'label': 'National', 'value': 'US'}]
+
+
 # Set the initial value for the county dropdown when a new state is selected
-@app.callback(Output('county-dropdown', 'value'),
-              [Input('county-dropdown', 'options')])
-def set_county_value(available_options):
-    return available_options[0]['value']
+# @app.callback(Output('county-dropdown', 'value'),
+#               [Input('county-dropdown', 'options')])
+# def set_county_value(available_options):
+#     return available_options[0]['value']
 
 
 if __name__ == '__main__':
--- Dashboard_Helper.py
+++ Dashboard_Helper.py
@@ -2,6 +2,7 @@ import pandas as pd
 from urllib.request import urlopen
 import json
 import plotly.graph_objects as go
+from plotly.graph_objs import Layout
 from os.path import exists
 import pickle
 import us
@@ -54,38 +55,70 @@ def make_deaths_figure(df, selected_state=None):
     if selected_state:
         filt = [x in selected_state for x in df['abbr']]
         real_deaths = df[filt].iloc[:,2:].sum().to_numpy()
+        fig_title = us.states.lookup(selected_state).name
     else:
         real_deaths = df.iloc[:,2:].sum().to_numpy()
+        fig_title = 'US National'
     dates = np.arange(np.ceil(len(df.iloc[0])/7))
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dates[:-4], y=real_deaths[:-28:7], mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=dates[-4:], y=real_deaths[-28::7], mode='lines+markers'))
+    fig = go.Figure(layout=Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)'))
+    fig.add_trace(go.Scatter(x=dates[:-4], y=real_deaths[:-28:7], name='True', mode='lines+markers', marker={'color':'lightgrey'},
+                             hovertemplate='<b>%{y:,}</b> <i>Actual</i><extra></extra>'))
+    fig.add_trace(go.Scatter(x=dates[-4:], y=real_deaths[-28::7], name='Predicted', mode='lines+markers',
+                             hovertemplate='<b>%{y:,}</b> <i>Predicted</i><extra></extra>'))
+    fig.add_shape(type='rect', xref='x', yref='paper', x0=-2, y0=0, x1=dates[-5], y1=1, line={'width': 0},
+                  fillcolor='#edeff2', layer='below')
     fig.update_layout(margin={'l':0, 'r':0, 't': 0, 'b': 0}, xaxis_title='Week', yaxis_title='Deaths',
-                      showlegend=False)
+                      showlegend=False, spikedistance=-1, hovermode='x')
+    # Set a vertical spike to show current time period
+    fig.update_xaxes(showgrid=False, zeroline=False, spikemode='across', spikesnap='cursor', spikecolor='black', spikedash='solid', spikethickness=1)
+    fig.update_yaxes(showgrid=False)
     return fig
 
 
 # Creates a figure with a state highlighted
-def make_map_figure(df, selected_state=None):
-    if selected_state:
-        locs = [selected_state]
-        deaths = [df[df['abbr'] == x].iloc[:, -1].to_numpy()[0] for x in [selected_state]]
-        customdata = np.dstack((deaths,us.states.lookup(selected_state).name))[0]
-    else:
-        locs = state_abbrevs
-        deaths = [df[df['abbr'] == x].iloc[:, -1].to_numpy()[0] for x in state_abbrevs]
-        customdata = np.dstack((deaths,state_names))[0]
-    fig = go.Figure(data=go.Choropleth(
-        locations=locs,
+def make_map_figure(df, selected_state=None, hover_state=None):
+    # For hover tooltip
+    deaths = [df[df['abbr'] == x].iloc[:, -1].to_numpy()[0] for x in state_abbrevs]
+    custom_data = np.dstack((deaths,state_names))[0]
+
+    # For color scaling
+    color_scale = 'Portland'
+    z_max = np.max(np.log10(deaths))
+    z_min = np.min(np.log10(deaths))
+
+    # Create the figure
+    fig = go.Figure()
+    fig.add_trace(go.Choropleth(
+        locations=state_abbrevs,
         z=np.log10(deaths),
-        customdata=customdata,
+        customdata=custom_data,
         hovertemplate='%{customdata[0]:,}<extra>%{customdata[1]}</extra>',
         locationmode='USA-states',
-        colorscale='Reds',
-        showscale=False
+        marker={'line': {'width': 0}},
+        marker_line_color=None,
+        #showlakes=False,
+        colorscale=color_scale,
+        colorbar={'title': 'Deaths', 'tickvals': [1,2,3,4], 'ticktext': ['10','100','1,000','10,000'],
+                  'xanchor': 'left', 'x': 0.9, 'yanchor': 'middle'}
     ))
-    fig.update_layout(margin={'l':0, 'r':0, 't': 0, 'b': 0}, geo_scope='usa')
+    # If a state is selected, highlight it
     if selected_state:
-        fig.update_geos(fitbounds='locations')
-
+        selected_data = [[df[df['abbr'] == selected_state].iloc[:, -1].to_numpy()[0],
+                          us.states.lookup(selected_state).name]]
+        fig.add_trace(go.Choropleth(
+            locations=[selected_state],
+            z=[np.log10(selected_data[0][0])],
+            zmin=z_min,
+            zmax=z_max,
+            customdata=selected_data,
+            hovertemplate='%{customdata[0]:,}<extra>%{customdata[1]}</extra>',
+            locationmode='USA-states',
+            marker={'line': {'width': 2}},
+            marker_line_color='black',
+            colorscale=color_scale,
+            showscale=False
+        ))
+    fig.update_layout(margin={'l':0, 'r':0, 't': 0, 'b': 0}, geo_scope='usa', dragmode=False)
+    #if selected_state:
+    #    fig.update_geos(fitbounds='locations')
     return fig
